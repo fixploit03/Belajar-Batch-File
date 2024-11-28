ea244ad439fce5598afb05ba011db1af969b2ea7
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2024-11-28 20:24:22 +0700

    Add files via upload

---
+++ komentar.cmd
@@ -0,0 +1,32 @@
+@echo off
+:: Belajar Batch File
+:: dibuat oleh: Rofi
+::
+:: Cara menulis komentar dalam Batch File
+::
+:: Anda dapat menulis komentar menggunakan dua cara
+:: yaitu dengan 'rem' dan dengan '::'. Berikut ini
+:: penjelasan untuk masing-masing metode:
+::
+:: 1. Menggunakan rem
+::    rem adalah singkatan dari "remark", yang 
+::    digunakan untuk menulis komentar dalam 
+::    Batch File.
+::
+:: Contohnya:
+::
+:: rem Ini adalah komentar
+echo Belajar Batch File itu menyenangkan!
+::
+:: 2. menggunakan ::
+::    '::' juga digunakan untuk menulis komentar 
+::    dalam Batch File. Ini adalah metode yang
+::    lebih ringkas dan sering digunakan oleh
+::    para pengguna Batch File.
+::
+:: Contohnya:
+::
+:: :: Ini adalah komentar
+echo Belajar Batch File itu menyenangkan!
+::
+pause
